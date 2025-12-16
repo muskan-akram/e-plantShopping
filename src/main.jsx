3a0d42875d5e72b7e5e0bbf3fa6569ede795458e
Muskan <muskanakram041@gmail.com>
2025-12-16 22:53:01 +0500

    Task 6 Completed: Set up the global store by updating main.jsx and also changes styling

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* The application is now wrapped with the Redux Provider, 
+        making the global 'store' available to all components. */}
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
 )
